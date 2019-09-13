97dc5733bd28e3cace4da53a32ec53d4442997e5
Wyatt Weisensel <wweisensel@gmail.com>
2019-09-13 22:40:30 +0900

    All errors get sent as an array

--- handlers/error.js
+++ handlers/error.js
@@ -1,9 +1,19 @@
-// Middleware for handing errors
+// Middleware for handing errors. Always send errors as an array for consistency.
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, request, response, next) { // next param not being used. Express needs to see this as a function with 4 params
+    let errMsg;
+
+    if (error.message && typeof error.message === 'string') {
+        errMsg = [error.message];
+    } else if (!error.message) {
+        errMsg = 'Oops! Something went wrong.';
+    } else {
+        errMsg = error.message;
+    }
+
     return response.status(error.status || 500).json({
         error: {
-            message: error.message || 'Oops! Something went wrong.',
+            message: errMsg,
         },
     });
 }
