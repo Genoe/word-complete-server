19de30165d6944c0f611b634188284e60c906243
Wyatt Weisensel <wweisensel@gmail.com>
2019-09-13 20:17:39 +0900

    Added tests for user signup

--- test/users.test.js
+++ test/users.test.js
@@ -23,10 +23,78 @@ describe('api/auth', () => {
                     password: 'morepotatoes',
                     email: 'potatoes@potatoes.com',
                 });
+
             expect(res.status).to.equal(200);
             expect(res.body).to.have.property('username', 'potatoes');
             expect(res.body).to.have.property('id');
             expect(res.body).to.have.property('token');
+            expect(res.body).to.have.property('email', 'potatoes@potatoes.com');
+        });
+
+        it('Should prompt for a longer password', async () => {
+            const res = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: 'potatoes',
+                    password: '1234567',
+                    email: 'potatoes@potatoes.com',
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Password Must Be At Least 8 Characters Long');
+        });
+
+        it('Should prompt for a longer username', async () => {
+            const res = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: 'po',
+                    password: '12345678',
+                    email: 'potatoes@potatoes.com',
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Username Must Be Between 5 and 20 Characters');
+        });
+
+        it('Should prompt for a vaild email address', async () => {
+            const res = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: 'potatoes',
+                    password: '12345678',
+                    email: 'potatoes@potatoes',
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Please Enter a Valid Email Address');
+        });
+
+        it('Should prompt for a vaild email, username, and password', async () => {
+            const res = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: '',
+                    password: '',
+                    email: 'potatoes@potatoes',
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(3).that
+                .includes('Please Enter a Valid Email Address')
+                .and.includes('Username Must Be Between 5 and 20 Characters')
+                .and.includes('Password Must Be At Least 8 Characters Long');
         });
     });
 });
