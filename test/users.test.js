ea1b4003a96770018927e0a27dbf3074aaddc5b4
Wyatt Weisensel <wweisensel@gmail.com>
2019-10-30 21:48:25 +0900

    tests for bad or no jwt

--- test/users.test.js
+++ test/users.test.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 process.env.DB_URL = 'mongodb://localhost/word-complete-test';
-process.env.PORT = 8090;
+process.env.PORT = 9090;
 
 const request = require('supertest');
 const { expect } = require('chai');
@@ -194,6 +194,142 @@ describe('api/auth', () => {
             expect(res.body.error).to.have.property('message');
             expect(res.body.error.message).to.be.an('array').of.length(1).that
                 .includes('Invalid Email/Password');
+        });
+    });
+});
+
+describe('api/users', () => {
+    describe('POST api/users/id/account', () => {
+        let token;
+        let id;
+
+        before(async () => {
+            await User.deleteMany({});
+
+            // create the user that gets updated
+            const res = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: 'testing',
+                    password: 'potatoes',
+                    email: 'potatoes@potatoes.com',
+                });
+
+            token = res.body.token;
+            id = res.body.id;
+
+            // create an account that already uses a desired new username
+            await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    username: 'taken',
+                    password: 'potatoes',
+                    email: 'taken@potatoes.com',
+                });
+        });
+
+        after(async () => {
+            await User.deleteMany({});
+        });
+
+        it('Should update the username', async () => {
+            console.log(token, id);
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: 'new username',
+                })
+                .set({
+                    Authorization: `Bearer ${token}`,
+                });
+
+            expect(res.status).to.equal(200);
+            expect(res.body).to.have.property('id');
+            expect(res.body).to.have.property('username', 'new username');
+            expect(res.body).to.have.property('email');
+            expect(res.body).to.have.property('token');
+        });
+
+        it('Should say the username is too long/short (long)', async () => {
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: 'new username this username is too long',
+                })
+                .set({
+                    Authorization: `Bearer ${token}`,
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Username Must Be Between 5 and 20 Characters');
+        });
+
+        it('Should say the username is too long/short (short)', async () => {
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: '',
+                })
+                .set({
+                    Authorization: `Bearer ${token}`,
+                });
+
+            expect(res.status).to.equal(422);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Username Must Be Between 5 and 20 Characters');
+        });
+
+        it('Should say the username already exists', async () => {
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: 'taken',
+                })
+                .set({
+                    Authorization: `Bearer ${token}`,
+                });
+
+            expect(res.status).to.equal(400);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Sorry, that username is taken');
+        });
+
+        it('Should say to log in (bad token)', async () => {
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: 'taken',
+                })
+                .set({
+                    Authorization: `Bearer ${token}bad`, // add nonsense to the token
+                });
+
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Please log in first');
+        });
+
+        it('Should say to log in (no token)', async () => {
+            const res = await request(app)
+                .put(`/api/users/${id}/account`)
+                .send({
+                    username: 'taken',
+                });
+
+            expect(res.status).to.equal(401);
+            expect(res.body).to.have.property('error');
+            expect(res.body.error).to.have.property('message');
+            expect(res.body.error.message).to.be.an('array').of.length(1).that
+                .includes('Please log in first');
         });
     });
 });
