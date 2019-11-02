348de5e8f6714c91be8f6485c0920468a87b9e1f
Wyatt Weisensel <wweisensel@gmail.com>
2019-11-03 00:34:28 +0900

    users can connect and get matched with another player and start sending messages. Crashes due to issues with handling diconnecting players

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const io = require('socket.io')();
 const errorHandler = require('./handlers/error');
 const authRoutes = require('./routes/auth');
 const accountRoutes = require('./routes/account');
 const { loginRequired, ensureCorrectUser } = require('./middleware/auth');
 
 const app = express();
+let users = {};
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -28,5 +30,56 @@ app.use(errorHandler);
 app.listen(process.env.PORT, () => {
     console.log(`Server has started on port ${process.env.PORT}`);
 });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    // TODO: Switch to arrow functions
+    socket.on('username', (username) => {
+        console.log(`User: ${username} has connected`);
+        // let matchedUserIndex = users.findIndex(function (element) {
+        //   return element.pending
+        // });
+        const matchedUserId = Object.keys(users).find((key) => users[key].pending);
+        users[socket.id] = {
+        // id: socket.id,
+            username,
+            pending: true, // is this user waiting to be matched up? TODO: Just rely on opponentId being false/falsy?
+            oppenentId: null, // When users disconnect, delete that user and set their opponent's pending to true and opponentId to null
+        };
+        if (matchedUserId) { // TODO: Handle people leaving but not others joining. Match w/ others who's opponent also left
+            socket.emit('pending', `You have been matched with ${users[matchedUserId].username}`);
+            socket.emit('match found', users[matchedUserId].username);
+            socket.broadcast.to(matchedUserId).emit('pending', `You have been matched with ${username}`);
+            socket.broadcast.to(matchedUserId).emit('match found', username);
+            users[socket.id].oppenentId = matchedUserId;
+            users[socket.id].pending = false;
+            users[matchedUserId].oppenentId = socket.id;
+            users[matchedUserId].pending = false;
+        } else {
+            socket.emit('pending', `Hello ${username}. Plese wait for our matchmaking sauce to finish...`);
+        }
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log(`message: ${msg}`);
+        socket.broadcast.to(users[socket.id].oppenentId).emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        // const index = users.findIndex(element => element.id === socket.id);
+        if (users[socket.id].oppenentId) {
+            users[users[socket.id].oppenentId].pending = true;
+            socket.broadcast.to(users[socket.id].oppenentId).emit('opponent disconnected');
+            // TODO: Set opponent back to pending. Alert them.
+        }
+        delete users[socket.id];
+
+        console.log('user disconnected');
+        console.log(`Connected Users: ${JSON.stringify(users)}`);
+    });
+});
+
+io.listen(process.env.IO_PORT);
+console.log('socket.io listening on port ', process.env.IO_PORT);
 
 module.exports = app;
