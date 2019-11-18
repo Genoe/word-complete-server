9dff49556beab5bedc5e4e052a8d67702a97d62e
Wyatt Weisensel <wweisensel@gmail.com>
2019-11-18 20:10:06 +0900

    extracted word checking logic to separate functions

--- socketServer.js
+++ socketServer.js
@@ -18,6 +18,67 @@ fs.readFile('./OfficialCrosswords.txt', (err, data) => {
 });
 
 /**
+ * Check if the word starts with the opponents last word's ending letter
+ * @param {String} msg // the chat message
+ * @param {String} id // SocketIO id of the player
+ * @param {String} oppId // SocketIO id of the opponent
+ */
+function isCharValid(msg, oppId) {
+    const { lastWord } = users[oppId];
+
+    if (lastWord) {
+        const lastChar = lastWord.slice(lastWord.length - 1);
+
+        return lastChar === msg[0];
+    }
+
+    return true;
+}
+
+/**
+ * Make sure the incoming chat message is valid. If not,
+ * return an object with messages and who's turn info for each player
+ * @param {String} msg // the chat message
+ * @param {String} id // SocketIO id of the player
+ * @param {String} oppId // SocketIO id of the opponent
+ */
+function isBadWord(msg, id, oppId) {
+    const oppUsername = users[oppId].username;
+    const { username } = users[id];
+
+    const result = {
+        isValid: true,
+    };
+
+    if (!words.has(msg)) {
+        result.isValid = false;
+
+        result[id] = {};
+        result[id].msg = `${msg} is not a word! Lose a turn!`;
+        result[id].isTurn = false;
+
+        result[oppId] = {};
+        result[oppId].msg = `${oppUsername} said ${msg} which is not a word! Your turn!`;
+        result[oppId].isTurn = true;
+    } else if (!isCharValid(msg, oppId)) {
+        const { lastWord } = users[oppId];
+        const oppLastLetter = users[oppId].lastWord.slice(lastWord.length - 1);
+
+        result.isValid = false;
+
+        result[id] = {};
+        result[id].msg = `${msg} does not begin with ${oppLastLetter}! Lose a turn!`;
+        result[id].isTurn = false;
+
+        result[oppId] = {};
+        result[oppId].msg = `${username} said ${msg} which does not begin with ${oppLastLetter}! Your turn!`;
+        result[oppId].isTurn = true;
+    }
+
+    return result;
+}
+
+/**
  * Set up all events for the Socket IO server
  * @param {SocketIO.Server} io
  */
@@ -38,6 +99,7 @@ function ioServer(io) {
                 pending: true, // is this user waiting to be matched up? TODO: Just rely on opponentId being false/falsy?
                 oppenentId: null, // When users disconnect, delete that user and set their opponent's pending to true and opponentId to null
                 isTurn: null,
+                lastWord: null,
             };
             if (matchedUserId) {
                 socket.emit(
@@ -80,35 +142,40 @@ function ioServer(io) {
             }
         });
 
-        socket.on('chat message', (msg) => {
+        socket.on('chat message', (rawMsg) => {
             const { oppenentId, isTurn } = users[socket.id];
-            const opponentUsername = users[oppenentId].username;
-            const lwrcsMsg = msg.toLowerCase();
+            const msg = rawMsg.toLowerCase();
 
             console.log('USERS_CHAT_MESSAGE', JSON.stringify(users));
-            console.log(`message: ${lwrcsMsg}`);
+            console.log(`message: ${msg}`);
 
-            // Do nothing if it is not their turn
+            // Do nothing if it is not their turn. As of now, this should only be possible if they are messing
+            // with the game in the browser or using other tools.
             if (!isTurn) {
                 console.log('Player submitted when it was not their turn!');
-            } else if (words.has(lwrcsMsg)) {
-                socket.broadcast.to(oppenentId).emit('chat message', lwrcsMsg);
             } else {
                 // user can lose a turn if it's not a word or doesn't match the ending letter of the previous word
-                socket.emit(
-                    'bad word',
-                    {
-                        msg: `${lwrcsMsg} is not a word! Lose a turn!`,
-                        isTurn: false,
-                    },
-                );
-                socket.broadcast.to(oppenentId).emit(
-                    'bad word',
-                    {
-                        msg: `${opponentUsername} said ${lwrcsMsg} which is not a word! Your turn!`,
-                        isTurn: true,
-                    },
-                );
+                const result = isBadWord(msg, socket.id, oppenentId);
+
+                if (result.isValid) {
+                    socket.broadcast.to(oppenentId).emit('chat message', msg);
+                    users[socket.id].lastWord = msg;
+                } else if (!result.isValid) {
+                    socket.emit(
+                        'bad word',
+                        {
+                            msg: result[socket.id].msg,
+                            isTurn: result[socket.id].isTurn,
+                        },
+                    );
+                    socket.broadcast.to(oppenentId).emit(
+                        'bad word',
+                        {
+                            msg: result[oppenentId].msg,
+                            isTurn: result[oppenentId].isTurn,
+                        },
+                    );
+                }
             }
 
             if (isTurn) {
