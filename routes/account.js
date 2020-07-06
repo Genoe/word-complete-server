74e925ebec642779d9aef45451bdb6d436232199
Wyatt Weisensel <wweisensel@gmail.com>
2020-07-06 13:27:25 -0500

    Using validateFields middleware for updating username

--- routes/account.js
+++ routes/account.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const validations = require('./validationRules');
 const { updateUsername } = require('../handlers/account');
+const { validateFields } = require('../middleware/validation');
 
 const router = express.Router({ mergeParams: true });
 
 // prefix: /api/users/:id/  -> mergeParams allows us to get the :id paramter from the parent route
-router.route('/').put([validations[2]], updateUsername);
+router.route('/').put([validations[2]], validateFields, updateUsername);
 
 module.exports = router;
